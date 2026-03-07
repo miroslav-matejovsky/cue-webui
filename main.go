a8a92546b8e2fc4a82bad175b2a3b8f5f0417f23
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2026-03-07 06:19:32 +0100

    playing with cue

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	_ "embed"
+	"fmt"
+
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/cuecontext"
+)
+
+//go:embed schema.cue
+var schemaFile string
 
 func main() {
-	fmt.Println("Hello, CUE WebUI!")
+	ctx := cuecontext.New()
+	rootValue := ctx.CompileString(schemaFile)
+
+	v1 := rootValue.LookupPath(cue.ParsePath("#V1"))
+	v2 := rootValue.LookupPath(cue.ParsePath("#V2"))
+	v3 := rootValue.LookupPath(cue.ParsePath("#V3"))
+
+	fmt.Println("V2 is backwards compatible with V1:", v2.Subsume(v1) == nil)
+	fmt.Println("V3 is backwards compatible with V2:", v3.Subsume(v2) == nil)
 }
